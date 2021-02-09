bf2bdb83926b0c3073ea45d7cb16f9a022f346df
Shuta Uehara <s20003@std.it-college.ac.jp>
2021-02-09 06:36:57 +0000

    basic part

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isLoaded: false,
+      items: []
+    }
+    this.URI = 'http://api.open-notify.org/iss-now.json'
+  }
+
+  componentDidMount () {
+    window
+      .fetch(this.URI)
+      .then(res => res.json())
+      .then(json => {
+        console.log(json.iss_position)
+        this.setState({ isLoaded: true, items: json.iss_position })
+      })
+  }
+
+  render () {
+    const { items, isLoaded } = this.state
+    console.log(items)
+    if (!isLoaded) {
+      return <div>...Loading</div>
+    } else {
+      return (
+        <div>
+          <ul>
+            {Object.keys(items).map(key => (
+              <li key={key}>{key} - {items[key]}</li>
+            ))}
+          </ul>
+        </div>
+      )
+    }
+  }
+}
+
+export default App
